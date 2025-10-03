fd478b2930a43fd928a703f28b54a8a9afe53ecc
xuanbach-dev <44861409+xuanbach-dev@users.noreply.github.com>
2025-10-03 10:05:18 +0700

    update view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 import Navbar from './components/Navbar';
 import BottomNavbar from './components/BottomNavbar';
@@ -38,6 +39,7 @@ function App() {
         <BottomNavbar />
         <Footer />
         <SpeedInsights />
+        <Analytics />
       </div>
     </Router>
   );
